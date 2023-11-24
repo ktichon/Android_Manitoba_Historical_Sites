81dce216b26b21d5f8388a30d830c4b7110b34c0
ktichon <ktichon@icloud.com>
2023-11-24 12:10:27 -0600

    Updated Settings to display current value

--- app/src/main/java/com/example/manitobahistoricalsites/SettingsFragment.java
+++ app/src/main/java/com/example/manitobahistoricalsites/SettingsFragment.java
@@ -9,9 +9,14 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.manitobahistoricalsites.HolderClasses.DisplayMode;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 import top.defaults.colorpicker.ColorPickerPopup;
 
@@ -24,6 +29,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     private EditTextPreference pSecondaryColour;
     private EditTextPreference pTextColour;
+
+
 
 
     @Override
@@ -44,6 +51,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         pBackgroundColour = findPreference(getString(R.string.background_colour_key));
         mainView.setBackgroundColor(Color.parseColor(pBackgroundColour.getText()));
+        pBackgroundColour.setSummary(pBackgroundColour.getText());
 
         pBackgroundColour.setOnBindEditTextListener(editText -> new ColorPickerPopup.Builder(getContext())
                 .initialColor(Color.parseColor(pBackgroundColour.getText())) // Set initial color
@@ -60,12 +68,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                         mainView.setBackgroundColor(colour);
                         editText.setText (String.format("#%06X", (0xFFFFFF & colour)));
                         pBackgroundColour.setText(String.format("#%06X", (0xFFFFFF & colour)));
+                        pBackgroundColour.setSummary(String.format("#%06X", (0xFFFFFF & colour)));
                     }
 
 
                 }));
 
         pSecondaryColour = findPreference(getString(R.string.secondary_colour_key));
+        pSecondaryColour.setSummary(pBackgroundColour.getText());
 
         pSecondaryColour.setOnBindEditTextListener(editText -> new ColorPickerPopup.Builder(getContext())
                 .initialColor(Color.parseColor(pSecondaryColour.getText())) // Set initial color
@@ -83,12 +93,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                         editText.setText (String.format("#%06X", (0xFFFFFF & colour)));
                         editText.setBackgroundColor(colour);
                         pSecondaryColour.setText(String.format("#%06X", (0xFFFFFF & colour)));
+                        pSecondaryColour.setSummary(String.format("#%06X", (0xFFFFFF & colour)));
                     }
 
 
                 }));
 
         pTextColour = findPreference(getString(R.string.text_colour_key));
+        pTextColour.setSummary(pBackgroundColour.getText());
         pTextColour.setOnBindEditTextListener(editText -> new ColorPickerPopup.Builder(getContext())
                 .initialColor(Color.parseColor(pTextColour.getText())) // Set initial color
                 .enableBrightness(true) // Enable brightness slider or not
@@ -104,6 +116,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                         editText.setTextColor(colour);
                         editText.setText (String.format("#%06X", (0xFFFFFF & colour)));
                         pTextColour.setText(String.format("#%06X", (0xFFFFFF & colour)));
+                        pTextColour.setSummary(String.format("#%06X", (0xFFFFFF & colour)));
 
 
                     }
@@ -114,7 +127,50 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         CheckBoxPreference pUpdateMarkers = findPreference(getString(R.string.update_marker_key));
         pUpdateMarkers.setChecked(false);
 
+        ListPreference lpBuilding = findPreference("Building");
+        lpBuilding.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpBuilding.setSummary(getColourName(lpBuilding.getValue()));
 
+        ListPreference lpCemetery= findPreference("Cemetery");
+        lpCemetery.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpCemetery.setSummary(getColourName(lpCemetery.getValue()));
+
+        ListPreference lpLocation = findPreference("Location");
+        lpLocation.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpLocation.setSummary(getColourName(lpLocation.getValue()));
+
+        ListPreference lpMonument = findPreference("Monument");
+        lpMonument.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpMonument.setSummary(getColourName(lpMonument.getValue()));
+
+        ListPreference lpMuseumOrArchives = findPreference("Museum or Archives");
+        lpMuseumOrArchives.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpMuseumOrArchives.setSummary(getColourName(lpMuseumOrArchives.getValue()));
+
+        ListPreference lpOther = findPreference("Other");
+        lpOther.setOnPreferenceChangeListener(onPreferenceChangeListener);
+        lpOther.setSummary(getColourName(lpOther.getValue()));
+    }
+
+    Preference.OnPreferenceChangeListener onPreferenceChangeListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+            preference.setSummary(getColourName(newValue.toString()));
+            return false;
+        }
+    };
+
+    
+    //Gets the colour name from the colour value
+    public String getColourName (String colourValue)
+    {
+        String [] colourName = getResources().getStringArray(R.array.Colour_Names);
+        String [] colourValues = getResources().getStringArray(R.array.Colour_Values);
+        int position = Arrays.asList(colourValues).indexOf(colourValue);
+        String displayColour = "HUE_RED";
+        if (position > -1 && position < colourName.length)
+            displayColour = colourName[position];
+        return displayColour;
     }
 
     @Override
